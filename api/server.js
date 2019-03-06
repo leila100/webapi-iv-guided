584e186a8b212880f9ca7c81b44b73805d7fc7e3
Leila <nb.leila10@gmail.com>
2019-03-06 14:11:19 -0600

    Add MOT environment variable

--- api/server.js
+++ api/server.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express")
+const helmet = require("helmet")
 
-const db = require('../data/db.js');
+const db = require("../data/db.js")
 
-const server = express();
+const server = express()
 
-server.use(helmet());
-server.use(express.json());
+server.use(helmet())
+server.use(express.json())
 
-server.get('/', async (req, res) => {
+server.get("/", async (req, res) => {
   try {
-    const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const shoutouts = await db("shoutouts")
+    const mot = process.env.MOT || "No message of the day"
+    res.status(200).json({ messageOfTheDay: mot, shoutouts })
   } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+    console.error("\nERROR", error)
+    res.status(500).json({ error: "Cannot retrieve the shoutouts" })
   }
-});
+})
 
-server.post('/', async (req, res) => {
+server.post("/", async (req, res) => {
   try {
-    const [id] = await db('shoutouts').insert(req.body);
-    const shoutouts = await db('shoutouts');
+    const [id] = await db("shoutouts").insert(req.body)
+    const shoutouts = await db("shoutouts")
 
-    res.status(201).json(shoutouts);
+    res.status(201).json(shoutouts)
   } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
+    console.error("\nERROR", error)
+    res.status(500).json({ error: "Cannot add the shoutout" })
   }
-});
+})
 
-module.exports = server;
+module.exports = server
